f0bebab62945f9810e1962690cb69bdc8f2cb2f0
Juanje Ojeda <jojeda@emergya.es>
2012-01-07 07:06:31 +0100

    Changed Packages class to lookup for translations

--- lib/debsfromrepos/packages.rb
+++ lib/debsfromrepos/packages.rb
@@ -6,16 +6,22 @@ module DebsFromRepos
 
     attr_reader :packages
 
-    def initialize(url)
-      @url = url
-      get_packages
+    def initialize(packages_url, translations_url=nil)
+      get_packages(packages_url)
+      if translations_url
+        lang_match = translations_url.match(/Translation-([a-z][a-z]).gz/)
+        if lang_match
+          lang = "-#{lang_match[1]}"
+          get_packages(translations_url, lang)
+        end
+      end
     end
 
     private
-    def get_packages
-      @packages = Hash.new
+    def get_packages(url, lang="")
+      @packages ||= Hash.new
       begin
-        zip_file = Zlib::GzipReader.new(open(@url))
+        zip_file = Zlib::GzipReader.new(open(url))
         packages_lines = zip_file.readlines("\n\n")
       rescue Exception => e
         packages_lines = []
@@ -25,7 +31,7 @@ module DebsFromRepos
         next unless name_match
         name = name_match[1]
         description = ''
-        description_match = line.match(/Description:\s(.+)\n/)
+        description_match = line.match(/Description#{lang}:\s(.+)\n/)
         description = description_match[1] if description_match
         @packages[name] = description
       end
